b2c8d4763170b3793090bd03384782535839dcd7
Boniphace <boniphacebageme06@gmail.com>
2025-11-29 17:59:35 +0300

    polished my index.js codes using chatgpt to finalise the simon-game project

--- index.js
+++ index.js
@@ -1,49 +1,72 @@
 var buttonColors = ["red", "blue", "green", "yellow"];
-
+var userClickedButton = [];
 var gamePattern = [];
+var level = 0;
 
-function nextSequence(){
-  var randomNumber =Math.floor(Math.random()*4);
-  var randomChosenColor = buttonColors[randomNumber];
-  gamePattern.push(randomChosenColor);
-  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+// Click handler (attach once)
+$(".btn").click(function() {
+    var clickedButtonId = $(this).attr("id");
+    userClickedButton.push(clickedButtonId);
 
-  var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
-  audio.play();
+    // Button animation
+    $("#" + clickedButtonId).fadeOut(100).fadeIn(100);
+    $("#" + clickedButtonId).addClass("pressed");
+    setTimeout(function() {
+        $("#" + clickedButtonId).removeClass("pressed");
+    }, 100);
 
-  $(".btn").click(function() {
-  var clickedButtonId = $(this).attr("id");
-  $(this).fadeOut(100).fadeIn(100);
-  var audio = new Audio("sounds/" + clickedButtonId + ".mp3");
-  audio.play();
+    // Play sound
+    var audio = new Audio("sounds/" + clickedButtonId + ".mp3");
+    audio.play();
 
+    checkAnswer(userClickedButton.length - 1);
 });
 
+// Keypress to start game
+$(document).keypress(function() {
+    if(level === 0){
+        nextSequence();
+    }
+});
 
+// Generate next sequence
+function nextSequence() {
+    userClickedButton = [];
+    level++;
+    $("h1").text("Level: " + level);
+
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+
+    // Animate sequence button
+    $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+    $("#" + randomChosenColor).addClass("pressed");
+    setTimeout(function() {
+        $("#" + randomChosenColor).removeClass("pressed");
+    }, 100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
 
-$(document).keypress(function() {
-  nextSequence();
-});
+// Check user answer
+function checkAnswer(currentLevel) {
+    if(userClickedButton[currentLevel] === gamePattern[currentLevel]){
+        if(userClickedButton.length === gamePattern.length){
+            setTimeout(nextSequence, 1000);
+        }
+    } else {
+        var audio = new Audio("sounds/wrong.mp3");
+        audio.play();
+        $("h1").text("Game Over! Press any key to restart.");
+        startOver();
+    }
+}
 
-
-
-// switch (randomChosenColor) {
-//   case "red":
-//     alert("color is red")
-//     break;
-//
-//   case "blue":
-//     alert("color is blue")
-//     break;
-//
-//   case "green":
-//     alert("color is green")
-//     break;
-//
-//   case "yellow":
-//         alert("color is yellow")
-//         break;
-//   default:
-//
-// }
+// Reset game
+function startOver() {
+    level = 0;
+    gamePattern = [];
+    userClickedButton = [];
+}
